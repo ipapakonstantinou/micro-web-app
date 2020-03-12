dccaf95239ff3910d37861bc81d438ef93425483
johnrluther <johnypapakonstantinou@gmail.com>
2020-03-12 23:09:00 +0200

    Final

--- books/src/app (1).js
+++ books/src/app (1).js
@@ -9,13 +9,11 @@ app.get("/", (req, res) => {
   res.json({ msg: "books" });
 });
 
-// Search Book
 app.get("/api/v1/books", async (req, res) => {
   const books = await Book.find({});
   res.json(books);
 });
 
-// Create Book
 app.post("/api/v1/books", async (req, res) => {
   const book = new Book({ name: req.body.name });
   const savedBook = await book.save();
