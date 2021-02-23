cbd6974ea5142b6cb9e476f0b0fe048d8d307d0f
johnrluther <johnypapakonstantinou@gmail.com>
2021-02-23 20:16:31 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # TradingBot
-This is a attempt of trading bot for crypto
+  This is a attempt of trading bot for crypto
+
+
+# Informations
+  The base endpoint is: wss://stream.binance.com:9443
+  Streams can be accessed either in a single raw stream or in a combined stream
+  Raw streams are accessed at /ws/<streamName>
+
+# Examples
+  Stream Name: <symbol>@trade
+  wscat -c wss://stream.binance.com:9443/ws/btceur@trade
+  Stream Name: <symbol>@kline_<interval>
+  wscat -c wss://stream.binance.com:9443/ws/btceur@kline_1m
--- TradingBot.py
+++ TradingBot.py
@@ -9,56 +9,56 @@ load_dotenv(dotenv_path='./.config/.env')
 
 
 def load_config():
-    # If there is no env file ask for input
-    if not os.path.isfile('./.config/.env'):
-        api_key = input(str("Enter API Key: "))
-        from_currency = input(str("Enter from currency: "))
-        to_currency = input(str("Enter to currency: "))
-        symbol = input(str("Enter symbol: "))
-        market = input(str("Enter market: "))
-    else:
-        api_key = str(os.getenv('API_KEY'))
-        from_currency = str(os.getenv('FROM_CURRENCY'))
-        to_currency = str(os.getenv('TO_CURRENCY'))
-        symbol = str(os.getenv('SYMBOL'))
-        market = str(os.getenv('MARKET'))
+  # If there is no env file ask for input
+  if not os.path.isfile('./.config/.env'):
+    api_key = input(str("Enter API Key: "))
+    from_currency = input(str("Enter from currency: "))
+    to_currency = input(str("Enter to currency: "))
+    symbol = input(str("Enter symbol: "))
+    market = input(str("Enter market: "))
+  else:
+    api_key = str(os.getenv('API_KEY'))
+    from_currency = str(os.getenv('FROM_CURRENCY'))
+    to_currency = str(os.getenv('TO_CURRENCY'))
+    symbol = str(os.getenv('SYMBOL'))
+    market = str(os.getenv('MARKET'))
 
-    # Define Urls
-    url_now = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + from_currency + '&to_currency=' + to_currency + '&apikey=' + api_key
-    url_daily = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=' + symbol + '&market=' + market + '&apikey=' + api_key
+  # Define Urls
+  url_now = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + from_currency + '&to_currency=' + to_currency + '&apikey=' + api_key
+  url_daily = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=' + symbol + '&market=' + market + '&apikey=' + api_key
 
-    return url_now, url_daily
+  return url_now, url_daily
 
 def main():
 
-    # Load configurations
-    url_now, url_daily = load_config()
+  # Load configurations
+  url_now, url_daily = load_config()
 
-    # Request, Build and Display the Now Rate
-    data_now = json.loads(urllib.request.urlopen(url_now).read())
-    df_now = pd.DataFrame(data_now)
-    print(url_now)
-    display(df_now)
+  # Request, Build and Display the Now Rate
+  data_now = json.loads(urllib.request.urlopen(url_now).read())
+  df_now = pd.DataFrame(data_now)
+  print(url_now)
+  display(df_now)
 
-    # Build Daily Rates
-    data_daily = json.loads(urllib.request.urlopen(url_daily).read())
-    map = {'Date', '1a. open (EUR)', '1b. open (USD)', '2a. high (EUR)', '2b. high (USD)', '3a. low (EUR)', '3b. low (USD)', '4a. close (EUR)', '4b. close (USD)', '5. volume', '6. market cap (USD)'}
-    dict_daily = {}
-    df_daily = pd.DataFrame(columns=map)
+  # Build Daily Rates
+  data_daily = json.loads(urllib.request.urlopen(url_daily).read())
+  map = {'Date', '1a. open (EUR)', '1b. open (USD)', '2a. high (EUR)', '2b. high (USD)', '3a. low (EUR)', '3b. low (USD)', '4a. close (EUR)', '4b. close (USD)', '5. volume', '6. market cap (USD)'}
+  dict_daily = {}
+  df_daily = pd.DataFrame(columns=map)
+  value = []
+
+  for m in map:
+    print(m)
+    for date in data_daily['Time Series (Digital Currency Daily)']:
+      if m == 'Date':
+        value.append(date)
+      else:
+        value.append(data_daily['Time Series (Digital Currency Daily)'][date][m])
+    dict_daily[m] = value
     value = []
 
-    for m in map:
-        print(m)
-        for date in data_daily['Time Series (Digital Currency Daily)']:
-            if m == 'Date':
-                value.append(date)
-            else:
-                value.append(data_daily['Time Series (Digital Currency Daily)'][date][m])
-        dict_daily[m] = value
-        value = []
-
-    df_daily = pd.DataFrame(dict_daily)
-    print(url_daily)
-    display(df_daily)
+  df_daily = pd.DataFrame(dict_daily)
+  print(url_daily)
+  display(df_daily)
 
 main()
