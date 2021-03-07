8d0a9daa078a26c893423a75182ff5637772bd4f
johnrluther <johnypapakonstantinou@gmail.com>
2021-03-08 00:50:42 +0200

    Post buy option through API

--- app.py
+++ app.py
@@ -3,21 +3,25 @@ import sys, csv, talib
 import numpy as np
 import pandas as pd
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 from binance.client import Client
+from binance.enums import *
+
 
 # Append all the relevant paths
 sys.path.append('./.config')
+sys.path.append('./scripts')
 # Import the relevant scripts & config.py
 import config
 
 
 # Default parameters
 title = 'CoinView'
-
-
 # Create Flask application
 app = Flask(__name__)
+app.secret_key = b'dsfjfadohufphp357429hinfue297313hrufune'
+
+
 
 # Connect to the API
 client = Client(config.API_KEY, config.API_SECRET)
@@ -25,19 +29,35 @@ client = Client(config.API_KEY, config.API_SECRET)
 @app.route('/')
 def index():
 
-    info = client.get_account()
-    balances = info['balances']
-
+    print(request.form)
+    # Get balances
+    account = client.get_account()
+    balances = account['balances']
     df_balances = pd.DataFrame.from_dict(balances)
     df_balances = df_balances[['asset', 'free']].astype({'free': 'float'})
-    df_balances = df_balances[df_balances.free > 0]
-    print(df_balances)
+    df_balances = df_balances[df_balances.free != 0]
 
-    return render_template('index.html', title=title, balances=balances, df_balances=df_balances)
+    # Get the list of all available symbols
+    exchange_info = client.get_exchange_info()
+    symbols = exchange_info['symbols']
 
-@app.route('/buy')
+    return render_template('index.html', title = title, df_balances = df_balances, symbols = symbols)
+
+@app.route('/buy', methods=['POST'])
 def buy():
-    return 'buy'
+
+    print(request.form)
+
+    # try:
+    #     order = client.create_order(
+    #     symbol=request.form['symbol'],
+    #     side=SIDE_BUY,
+    #     type=ORDER_TYPE_MARKET,
+    #     quantity=request.form['quantity'])
+    # except Exception as e:
+    #     flash(e.message, "error")
+
+    return redirect('/')
 
 @app.route('/sell')
 def sell():
